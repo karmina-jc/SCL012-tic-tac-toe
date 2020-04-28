856db653b47a6915ba74391ee73f6df3ba177148
karmina-jc <karmina.jarac@gmail.com>
2020-04-28 19:06:01 -0400

    funtion to get winner and show winner on screen, button for new game

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Alert, Button } from 'react-native';
 import { MaterialCommunityIcons  as Icon } from 'react-native-vector-icons';
+import styles from './styles.js'
 
  function App() {
    
@@ -10,6 +11,45 @@ import { MaterialCommunityIcons  as Icon } from 'react-native-vector-icons';
   //estao inicial del turno del jugador
   const [player, setPlayer] = useState(1);
 
+  const [winner, setWinner] = useState(null)
+  
+  
+  const startState = () => {
+    setSquareState([[0,0,0],[0,0,0],[0,0,0]])
+
+    setWinner(null)
+  }
+
+  const getWinner =() => {
+    let sum
+    let arr = squareState
+
+    //revisando hileras...
+    for (let i = 0; i < 3; i++) {
+      sum=arr[i][0]+arr[i][1]+arr[i][2]
+      if(sum === 3) {return 1}
+      else if (sum === -3) {return -1}
+    }
+
+    //revisando columnas...
+    for (let i = 0; i < 3; i++) {
+      sum=arr[0][i]+arr[1][i]+arr[2][i]
+      if(sum === 3){return 1}
+      else if (sum === -3) {return -1}
+    }
+    //revisando diagonales...
+    sum=arr[0][0]+arr[1][1]+arr[2][2]
+    if(sum === 3){return 1}
+    else if(sum === -3){return -1}
+
+    sum=arr[2][0]+arr[1][1]+arr[0][2]
+    if(sum === 3){return 1}
+    else if(sum === -3){return -1}
+
+    //si no hay ganador...
+    return 0
+  }
+
   const handleClick = (row, col) => {
     //previene que cambie un recuadro ya usado
     if(squareState[row][col] !== 0) { return }
@@ -18,14 +58,25 @@ import { MaterialCommunityIcons  as Icon } from 'react-native-vector-icons';
     const squares = [...squareState]
     squares[row][col] = player
     setSquareState(squares)    
-    console.log(squares)
 
     //cambiar el turno del jugador
-    setPlayer((player === 1) ? -1 : 1 )
-  
+    setPlayer((player === 1) ? -1 : 1 )  
+
+    //se determinar al ganador...
+    let value = getWinner()
+    
+    if (value === 1) {
+      Alert.alert('the winnwr is Ada!')
+      startState()
+    }
+    else if(value === -1) {
+      Alert.alert('the winner is Amelia!')
+      startState()
+    }
+        
   }
 
-  //funcion para tomar coordenadas del tap y mostrar simbolo en pantalla
+  //funcion para tomar coordenadas del tap y mostrar imagen en pantalla
   const tapIcon = (row, col) => {
 
     const indSquare = squareState[row][col];
@@ -37,20 +88,22 @@ import { MaterialCommunityIcons  as Icon } from 'react-native-vector-icons';
     } 
   }
 
-
-  const startState = () => {
-    setSquareState([[0,0,0],[0,0,0],[0,0,0]])
-  }
+  const nextPlayer = () => {
+    switch(player){
+      case 1: return <Text>Ada</Text>
+      case -1: return <Text>Amelia</Text>
+      default: <Text/>
+    }    
+  }  
 
   return (
     <View style={styles.container}>
       <View style={styles.title}>
         <Text>Aqui va el Titulo del juego</Text>
-      </View>
-      
+      </View>      
       <View style={styles.gamebody}>
-        <View style={styles.player}>
-          <Text>Aqui va el truno del jugador</Text>
+        <View style={styles.player}>        
+          <Text>Player {nextPlayer()} </Text>
         </View>
         <View style={styles.squareline}>
           <TouchableOpacity style={styles.square} 
@@ -103,38 +156,13 @@ import { MaterialCommunityIcons  as Icon } from 'react-native-vector-icons';
             {tapIcon(2,2)}
           </TouchableOpacity> 
         </View> 
+        <Button
+        title="New Game"
+        onPress={() => startState()}
+        />
       </View>      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: '#BEADED',
-    flexDirection: 'column',
-  },
-  title: {
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-  },
-  player: {    
-  
-  },
-  gamebody: {
-    flex: 3,
-    alignItems: 'center'
-  },
-  square: {
-    borderWidth: 1,
-    width: 80,
-    height: 80,
-  },
-  squareline: {
-    flexDirection: "row",
-  }
-});
 
 export default App;
---
+++ styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      flex:1,
+      backgroundColor: '#BEADED',
+      flexDirection: 'column',
+    },
+    title: {
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      
+    },
+    alert: {
+      backgroundColor: 'green',
+
+    },
+    player: {    
+    
+    },
+    gamebody: {
+      flex: 3,
+      alignItems: 'center'
+    },
+    square: {
+      borderWidth: 1,
+      width: 80,
+      height: 80,
+    },
+    squareline: {
+      flexDirection: "row",
+    }
+  });
+  
